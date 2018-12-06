3a0beba6319bfe146a56e9e3ec7b4f2e85516df8
Dan Oppenheim <git@oppenheim.me.uk>
2018-12-06 12:50:35 +0000

    Convert single-diff to recursive function
    Also fixes some comments.

--- day02/day02_b.clj
+++ day02/day02_b.clj
@@ -1,23 +1,20 @@
 ; determine if there is one item difference between equal length sequences
-; todo: return to this to convert to a recursive function
-(defn single-diff [a b]
-    (loop [i 0  ;; character index
-           c 0] ;; number of differences
-        (let [ai (get a i) ; character in a at index i
-              bi (get b i) ; character in b at index i
-              len (count a)
-              c2 (if (not= ai bi) (inc c) c)] ; increment c if they're not equal
-            (cond 
-              (> c2 1) false ; can only have a single difference
-              (= i len) (= c2 1) ; if we've got to the end, return whether had a single difference
-              :else (recur (inc i) c2))))) ; otherwise loop to next character
+(defn single-diff [a b count]
+    (if (empty? a) (= count 1)
+        (let [aa (first a)
+              bb (first b)
+              c2 (if (not= aa bb) (inc count) count)]
+            (cond
+                (> c2 1) false ; can only have a single difference
+                :else (recur (rest a) (rest b) c2))))) ; recurse to rest of ids
+
 
 ; Checks id against every item in others to see if any combination has only
 ; a single difference. Returns the pair that match or false if none do.
 (defn check-id [id others]
     (cond
         (empty? others) false
-        (single-diff id (first others)) [id (first others)]
+        (single-diff id (first others) 0) [id (first others)]
         :else (recur id (rest others))))
 
 ; Recursive function that checks every box against every other to compare
@@ -30,10 +27,12 @@
             :else (recur (first others) (rest others)))))
 
 ; Returns a new list that is all of the matching characters in a and b
-; todo: There must be a nicer way of doing this?
 (defn to-common [a b]
     (map (fn [a b] (if (= a b) a)) a b))
 
+; Get the code by searching for the two ids that differ by a single letter
+; and return a string containing all of the matching letters or "not found" on
+; failure.
 (defn get-code [ids]
     (let [common (find-boxes (first ids) (rest ids))]
         (if (not common)
@@ -41,6 +40,6 @@
             (apply str (to-common (get common 0) (get common 1))))))
 
 (println
-    "The code is: "
+    "The code is:"
     (get-code (sort (clojure.string/split-lines (slurp "input.txt")))))
 
